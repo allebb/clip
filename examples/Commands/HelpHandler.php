18f5e36de57a989e92018c362dba30e3330c7f54
Bobby Allen <ballen@bobbyallen.me>
2016-01-20 20:42:58 +0000

    Corrects the help output to display the correct command name for the flags commands.

--- examples/Commands/HelpHandler.php
+++ examples/Commands/HelpHandler.php
@@ -36,7 +36,7 @@ class HelpHandler extends ConsoleApplication implements CommandInterface
         $this->writeln('Commands:');
         $this->writeln('  help       - This information screen');
         $this->writeln('  test       - A silly test example');
-        $this->writeln('  country    - Example using options.');
+        $this->writeln('  flags      - Example using options.');
         $this->writeln('  check:name - User input example');
         $this->writeln();
         $this->exitWithSuccess();
